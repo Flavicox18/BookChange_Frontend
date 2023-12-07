55845d378410bba34d4ce17affd16292fd747ec0
Flavio Villacorta <143514047+Flavicox18@users.noreply.github.com>
2023-12-07 03:22:15 -0500

    Arreglando Main

--- src/app/principal/main-principal/main-principal.component.ts
+++ src/app/principal/main-principal/main-principal.component.ts
@@ -16,23 +16,6 @@ export class MainPrincipalComponent implements OnInit{
   ngOnInit(): void {
     this.obtenerLibrosDisponibles();
   }
-  @ViewChild('carruselContainer') carruselContainer!: ElementRef;
-
-  elementosPorVista = 5;
-
-  libros = [
-    { titulo: '100 años de soledad', autor: 'Gabriel García Márquez', imagen: './assets/img/Libro1.webp' },
-    { titulo: 'Harry Potter y la piedra filosofal', autor: 'J. K. Rowling', imagen: './assets/img/Libro2.webp' },
-    { titulo: 'Los Juegos del Hambre en Llamas', autor: 'Suzzane Collins', imagen: './assets/img/Libro3.webp' },
-    { titulo: 'Don Quijote de la Mancha', autor: 'Miguel de Cervantes', imagen: './assets/img/Libro4.jpg' },
-    { titulo: 'El Principito', autor: 'Antoine de Saint-Exupéry', imagen: './assets/img/Libro5.webp' },
-  ];
-  moverCarrusel(direccion: number): void {
-    const container = this.carruselContainer.nativeElement as HTMLElement;
-    const scrollAmount = container.clientWidth / this.elementosPorVista * direccion;
-    container.scrollBy({ left: scrollAmount, behavior: 'smooth' });
-  }
-
   obtenerLibrosDisponibles(): void {
     this.librosService.listarLibrosDisponibles().subscribe(
       (libros: LibroLista[]) => {
